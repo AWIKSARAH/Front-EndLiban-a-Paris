016f12a7d1c578e7393ee0ceb60732ec7081b9ef
AWIKSARAH <awiksarah@gmail.com>
2023-04-22 23:45:47 +0300

    add the headers to the axios

--- src/common/axios.js
+++ src/common/axios.js
@@ -3,26 +3,26 @@ import axios from 'axios';
 
 axios.defaults.baseURL = process.env.API_BASE_URL;
 
-export const get = (endpoint, params = {}) => {
-  return axios.get(endpoint, { params })
+export const get = (endpoint, params = {},headers={}) => {
+  return axios.get(endpoint, { params },{headers})
     .then(response => response.data)
     .catch(error => console.log(error));
 };
 
-export const post = (endpoint, data) => {
-  return axios.post(endpoint, data)
+export const post = (endpoint, data,headers={}) => {
+  return axios.post(endpoint, data,{headers})
     .then(response => response.data)
     .catch(error => console.log(error));
 };
 
-export const put = (endpoint, data) => {
-  return axios.put(endpoint, data)
+export const put = (endpoint, data,headers={}) => {
+  return axios.put(endpoint, data , {headers})
     .then(response => response.data)
     .catch(error => console.log(error));
 };
 
-export const remove = (endpoint) => {
-  return axios.delete(endpoint)
+export const remove = (endpoint,headers={}) => {
+  return axios.delete(endpoint,{headers})
     .then(response => response.data)
     .catch(error => console.log(error));
 };
