ba96eb0316a7f390a5de13dd1bcdd29b3a2949a9
AWIKSARAH <awiksarah@gmail.com>
2023-04-27 16:03:56 +0300

    add logo to card

---
+++ src/components/listingcard/index.jsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import Card from './card'
+import "./listingCard.css"
+function linstingCard(props) {
+    return ( 
+
+        <div className="card--listing"style={{display:'grid'}}>
+        <Card image='https://thumbs.dreamstime.com/z/quinoa-tabbouleh-salad-fresh-tomatoes-peppers-cucumbers-148171773.jpg' title='hello' type='la wwalan wlakin walayassa walaw 7a7a kala joo7a fi zeti nahar ani layssa ladayahimar wa saazhab lil sook kala joo7a fi zeti nahar ani layssa ladayahimar wa saazhab lil sook kala joo7a fi zeti nahar ani layssa ladayahimar wa saazhab lil sook ' location='halba, Akkar'></Card>
+        <Card image='https://thumbs.dreamstime.com/z/quinoa-tabbouleh-salad-fresh-tomatoes-peppers-cucumbers-148171773.jpg' title='hello' description='la wwalan wlakin walayassa walaw 7a7a kala joo7a fi zeti nahar ani layssa ladayahimar wa saazhab lil sook kala joo7a fi zeti nahar ani layssa ladayahimar wa saazhab lil sook kala joo7a fi zeti nahar ani layssa ladayahimar wa saazhab lil sook ' location='halba, Akkar'></Card>
+        <Card image='https://thumbs.dreamstime.com/z/quinoa-tabbouleh-salad-fresh-tomatoes-peppers-cucumbers-148171773.jpg' title='hello' description='la wwalan wlakin walayassa walaw 7a7a kala joo7a fi zeti nahar ani layssa ladayahimar wa saazhab lil sook kala joo7a fi zeti nahar ani layssa ladayahimar wa saazhab lil sook kala joo7a fi zeti nahar ani layssa ladayahimar wa saazhab lil sook ' location='halba, Akkar'></Card>
+        <Card image='https://thumbs.dreamstime.com/z/quinoa-tabbouleh-salad-fresh-tomatoes-peppers-cucumbers-148171773.jpg' title='hello' description='la wwalan wlakin walayassa walaw 7a7a kala joo7a fi zeti nahar ani layssa ladayahimar wa saazhab lil sook kala joo7a fi zeti nahar ani layssa ladayahimar wa saazhab lil sook kala joo7a fi zeti nahar ani layssa ladayahimar wa saazhab lil sook ' location='halba, Akkar'></Card>
+
+        </div>
+
+     );
+}
+
+export default linstingCard;
