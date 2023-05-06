67dc5031b3d21139bc4b129030f37c00aa3dd475
ayadtarraf <tarraf_tarraf1111@hotmail.com>
2023-05-06 17:04:14 +0300

    almost-finished

--- src/components/RestaurantDetails/index.jsx
+++ src/components/RestaurantDetails/index.jsx
@@ -8,41 +8,53 @@ import {
   faInstagram,
   faTwitter,
 } from "@fortawesome/free-brands-svg-icons";
-const days =[
-  {
-    "french": "Lundi",
-    "english": "monday"
-  },
-  {
-    "french": "Mardi",
-    "english": "tuesday"
-  },
-  {
-    "french": "Mercredi",
-    "english": "wednesday"
-  },
-  {
-    "french": "Jeudi",
-    "english": "thursday"
-  },
-  {
-    "french": "Vendredi",
-    "english": "friday"
-  },
-  {
-    "french": "Samedi",
-    "english": "saturday"
-  },
-  {
-    "french": "Dimanche",
-    "english": "sunday"
-  }
-]
+import {
+  faCircleInfo,
+  faPhone,
+  faTag,
+  faEnvelope,
+  faCalendarDays
+} from "@fortawesome/free-solid-svg-icons";
 
+const days = [
+  {
+    french: "Lundi",
+    english: "monday",
+  },
+  {
+    french: "Mardi",
+    english: "tuesday",
+  },
+  {
+    french: "Mercredi",
+    english: "wednesday",
+  },
+  {
+    french: "Jeudi",
+    english: "thursday",
+  },
+  {
+    french: "Vendredi",
+    english: "friday",
+  },
+  {
+    french: "Samedi",
+    english: "saturday",
+  },
+  {
+    french: "Dimanche",
+    english: "sunday",
+  },
+];
+// const [showSchedule, setShowSchedule]=useState()
 function RestaurantDetails() {
   const { id } = useParams();
   const [restaurant, setRestaurant] = useState(null);
+  const [showSchedule, setShowSchedule] = useState(false);
 
+  const toggleSchedule = () => {
+    setShowSchedule(!showSchedule);
+  };
   useEffect(() => {
     axios
       .get(`http://localhost:5000/api/places/${id}`)
@@ -70,44 +82,78 @@ function RestaurantDetails() {
     schedule,
     placeType,
   } = restaurant;
-
+  
   return (
     <div className="restaurant-section">
       <div className="restaurant-container">
         <div className="restaurant-image">
-          <img src={image.startsWith("http")?image:`http://localhost:5000${image}`} alt={title}  width={400} height={300}/>
+          <img
+            className="img-rest"
+            src={
+              image.startsWith("http") ? image : `http://localhost:5000${image}`
+            }
+            alt={title}
+            width={1420}
+            height={300}
+          />
         </div>
-        <div className="restaurant-info">
-          <div className="restaurant-details">
+        <div className="restaurent-wrap">
+          <div className="restaurant-description-title">
             <h3 className="restaurant-title">{title}</h3>
+            <div className="restaurant-line"></div>
             <p className="restaurant-description">{description}</p>
-            <p className="restaurant-location">{location}</p>
-            <p className="restaurant-tel">{tel}</p>
-            <p className="restaurant-tags">{tags}</p>
-            <p className="restaurant-email">{email}</p>
-            <div className="restaurant-social-media">
-              {socialMedia.facebook && (
-                <a href={socialMedia.facebook}>
-                  <FontAwesomeIcon icon={faFacebook} size="2x" />
-                </a>
-              )}
-              {socialMedia.instagram && (
-                <a href={socialMedia.instagram}>
-                  <FontAwesomeIcon icon={faInstagram} size="2x" />
-                </a>
-              )}
-              {socialMedia.twitter && (
-                <a href={socialMedia.twitter}>
-                  <FontAwesomeIcon icon={faTwitter} size="2x" />
-                </a>
+          </div>
+          <div className="restaurant-info">
+            <div className="restaurant-details">
+              <p className="restaurant-location">
+                <FontAwesomeIcon icon={faCircleInfo} /> {location}
+              </p>
+              <p className="restaurant-tel">
+                <FontAwesomeIcon icon={faPhone} />
+                {tel}
+              </p>
+              <p className="restaurant-tags">
+                <FontAwesomeIcon icon={faTag} />
+                {tags}
+              </p>
+              <p className="restaurant-email">
+                <FontAwesomeIcon icon={faEnvelope} />
+                {email}
+              </p>
+              <div className="restaurant-social-media">
+                {socialMedia.facebook && (
+                  <a href={socialMedia.facebook}>
+                    <FontAwesomeIcon icon={faFacebook} size="2x" />
+                  </a>
+                )}
+                {socialMedia.instagram && (
+                  <a href={socialMedia.instagram}>
+                    <FontAwesomeIcon icon={faInstagram} size="2x" />
+                  </a>
+                )}
+                {socialMedia.twitter && (
+                  <a href={socialMedia.twitter}>
+                    <FontAwesomeIcon icon={faTwitter} size="2x" />
+                  </a>
+                )}
+              </div>
+
+              <button  className="schedual-button" onClick={toggleSchedule}><FontAwesomeIcon icon={faCalendarDays} /> Schedule</button>
+              {showSchedule && (
+                <div>
+                  {days.map((day) => (
+                    <div className="days-of-week_resto">
+                      <h4>{day.french}: </h4>
+                      <h4>
+                        {schedule[day.english].status === "open"
+                          ? schedule[day.english].fromTo
+                          : "Closed"}
+                      </h4>
+                    </div>
+                  ))}
+                </div>
               )}
             </div>
-            {days.map(day=>{
-              return <div className="days-of-week_resto">
-                <h4>{day.french}: {" "}</h4>{" "}
-                <h4>{schedule[day.english].status==="open"?schedule[day.english].fromTo:"Closed"}</h4>
-              </div>
-            })}
           </div>
         </div>
       </div>
